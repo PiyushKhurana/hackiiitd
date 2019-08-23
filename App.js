5ebd0081314e033347c28644b0a14a437cc7d3f2
AS <ananyavsrivastava@gmail.com>
2019-08-23 11:54:02 +0530

    Added CardFeed

--- App.js
+++ App.js
@@ -1,13 +1,6 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
-}
+import { StyleSheet, Text, View, ScrollView } from 'react-native';
+import CardFeed  from './CardFeed/cardfeed';
 
 const styles = StyleSheet.create({
   container: {
@@ -17,3 +10,38 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+class App extends React.Component {
+  render()
+  {
+  return (
+    <ScrollView>
+      <View style={styles.container}>
+    <Text>Open up App.js to start working on your app!</Text>
+    <CardFeed 
+      name='Ana Srivas' 
+      category='Diet' 
+      caption='Who said salads are not appetising??'
+      src='https://www.tasteofhome.com/wp-content/uploads/2017/10/exps6498_MRR133247D07_30_5b_WEB-2.jpg'/>  
+    <CardFeed 
+      name='Anushka Sharma' 
+      category='Exercise' 
+      caption='Planking is just for me:Practical And Simple!!'
+      src='https://res.cloudinary.com/fleetnation/image/private/c_fit,w_1120/g_south,l_text:style_gothic2:%C2%A9%20pakkawit%20anantaya,o_20,y_10/g_center,l_watermark4,o_25,y_50/v1535469198/trhm9s3q58xgibwhfde3.jpg'/>  
+    <CardFeed 
+      name='Ana Srivas' 
+      category='Diet' 
+      caption='I never undermine the importance of cheat days!!'
+      src='http://static.samacharjagatlive.com/newscdn/resources/uploads/ALL-NEWS/17012018/1516180131.jpg'/>  
+    <CardFeed 
+      name='Arnav Anand' 
+      category='Exercice' 
+      caption="Don't let the smile fool you! Took 30 minutes to click the right pic!"
+      src='https://strongains.com/wp-content/uploads/2014/05/Copy-of-dumbbell-lunges.jpg'/>  
+  </View>
+    </ScrollView>
+  );
+  }
+}
+
+
+export default App;
--- CardFeed/cardfeed.js
+++ CardFeed/cardfeed.js
@@ -1,33 +1,91 @@
 import React from 'react';
-import { StyleSheet, Text, View, TouchableHighlight,Image } from 'react-native';
+import { StyleSheet, Text, View, TouchableHighlight,Image,Animated, } from 'react-native';
 
 const styles = StyleSheet.create({
     container: {
-      flex: 1,
-      backgroundColor: '#fff',
-      alignItems: 'center',
+    //   flex: 1,
+      borderWidth:1,
+      borderColor:'#a0522d',
+      backgroundColor: '#ffefd5',
+    //   alignItems: 'center',
       justifyContent: 'center',
+      marginVertical:30,
     },
+    title:{
+        borderBottomColor:'#d3d3d3',
+        borderBottomWidth:1,
+        padding:5,
+        fontSize:15,
+        flexDirection:"row",
+    },
+    photoCont:{
+        borderRadius:50,
+        borderWidth: 1,
+        borderColor:'transparent',
+        overflow: 'hidden',
+    },
+    likeButton:{
+        alignItems: 'center',
+        justifyContent: 'center',
+        fontSize:25,
+        textAlign:'center',
+    }
   });
 
 class CardFeed extends React.Component {
+    animatedValue = new Animated.Value(0);
     constructor(props)
     {
         super(props);
         this.state=
         {
-            count=0
+            count:0,
+            text:'Like',
         };
+    }
+    
+    iLike=()=>
+    {
+        if(this.state.count===0)
+        {   this.setState({count:this.state.count+1});
+            this.setState({text:'Liked'}); 
+        Animated.timing(
+            this.animatedValue,
+            {
+                toValue:1,
+                duration:250
+            }
+        ).start()}
+        else if(this.state.count===1)
+        {   this.setState({count:this.state.count-1});
+        this.setState({text:'Like'}); 
+    Animated.timing(
+        this.animatedValue,
+        {
+            toValue:0,
+            duration:250
+        }
+    ).start()}
     }
     render()
     {
+    const bgColor=this.animatedValue.interpolate({
+        inputRange:[0,1],
+        outputRange:['#000000','#ff1493']
+    })
   return (
     <View style={styles.container}>
-        <Text>Ana Srivas posted in Diet</Text>
-        <Text>Who said salads are not appetising??</Text>
-        <Image style={{width: 300, height: 300}} source={{ 'https://www.tasteofhome.com/wp-content/uploads/2017/10/exps6498_MRR133247D07_30_5b_WEB-2.jpg'}} />
-        <TouchableHighlight >
-        <Text>Like</Text>
+        <View style={styles.title}>
+        <Text style={{fontWeight:'bold'}}>{this.props.name}</Text>
+        <Text> posted in </Text>
+        <Text style={{color:'#e9967a'}}>{this.props.category}</Text>
+        </View>
+        <Text style={styles.title}>{this.props.caption}</Text>
+        <View style={styles.photoCont}>
+        <Image style={[{width: 300, height: 300}]} source={{ uri:this.props.src}} />
+        </View>
+        <TouchableHighlight onPress={this.iLike}>
+        <Animated.Text style={[{color:bgColor},styles.likeButton]}>{this.state.text}</Animated.Text>
         </TouchableHighlight>
     </View>
   );
